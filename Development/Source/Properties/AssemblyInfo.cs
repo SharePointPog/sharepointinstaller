8ffb2e973fc9841f3ef227c8d74f118e1373f337
p <p@w.com>
2016-03-20 18:36:11 -0400

    Release 1.4.0.0

--- Development/Source/Properties/AssemblyInfo.cs
+++ Development/Source/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.14.0")]
-[assembly: AssemblyFileVersion("1.2.14.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
